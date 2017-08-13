e6c6986db3228b51c519cdb876e72a177b9add75
Umesh <Umesh@UMESH-TP2>
2017-08-13 13:35:33 -0400

    Fixed Defgect

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Test Prod On Server");
+            Console.WriteLine("Test Prod On Server Defect");
 
         }
     }
